a09383e872dc6c8775a3e8dcd57b10c4d9998c38
fabienrortais <fabienrortais@gmail.com>
2025-07-28 08:52:31 +0200

    Clean components and screens even more

--- main-front/src/services/index 2.ts
+++
@@ -1,9 +0,0 @@
-// Export all services
-export * from './apiClient';
-export * from './healthService';
-export * from './userService';
-
-// Default exports
-export { default as apiClient } from './apiClient';
-export { default as healthService } from './healthService';
-export { default as userService } from './userService';
